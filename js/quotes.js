35e36ff6f159968688b1fa72ff02aa0c8df429c6
Joshua Lee <configurellc@gmail.com>
2019-02-02 00:12:39 -0500

    refactored code. Created the quotes file which will host the data

---
+++ js/quotes.js
@@ -0,0 +1,22 @@
+
+function myQuotes() {
+    var quotes = [
+        {
+            quote: "Seven billion souls that move around the sun.",
+            source: "Childish Gambino",
+            citation: "Feels like summer"
+        },
+        {
+            quote: "Don't wait for your world to begin crumbling to make a change.",
+            source: "Joshua Lee",
+            citation: "Past midnight thoughts"
+        },
+        {
+            quote: "The only time we will ever have is right now, no other time exist.",
+            source: "Joshua Lee",
+            citation: "Past midnight thoughts"
+        }
+    ];
+
+    return quotes;
+}
