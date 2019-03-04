be451a4bd0f9c5e0a3fa2d2e9f27cf26cbb7288b
Joshua Lee <configurellc@gmail.com>
2019-03-04 17:44:09 -0500

    updated quotes

--- js/quotes.js
+++ js/quotes.js
@@ -22,6 +22,36 @@ function myQuotes() {
             source: "Patrick McKenzie",
             citation: "Twitter"
 
+        },
+        {
+            quote: "Create the highest, grandest vision possible for your life, because you become what you believe.",
+            source: "Author unknow",
+            citation: "Edlester.com"
+        },
+        {
+            quote: "You have as much laughter as you have faith.",
+            source: "Martin Luther King",
+            citation: "wisdom quotes"
+        },
+        {
+           quote: "Listen, smile, agree, and then do whaterver the f*** you were doing to do anyway.",
+           source: "Rober Downey Jr",
+           citation: "wisdom quotes" 
+        },
+        {
+            quote: "Always code as if the guy who ends up maintainig your code will be a violent pyscopath who knows where you live.",
+            source: "Martin Golding",
+            citation: "189 quotes for developers"
+        },
+        {
+            quote: "A computer program does what you tell it to do, not what you want it to do",
+            source: "Author Uknown",
+            citation: "189 quotes for developers"
+        },
+        {
+            quote: "If brute force doesn't solve your problems, then you aren't using enough",
+            source: "Author Uknown",
+            citation: "Twitter"
         }
     ];
     
